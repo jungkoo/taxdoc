551c74c5c55553c0aeb752289deb419ea9d976e8
mc.jeong <mc.jeong@navercorp.com>
2020-05-25 19:13:29 +0900

    등업기능 추가중

--- sample/naver_cafe_auto_join_sample.py
+++ sample/naver_cafe_auto_join_sample.py
@@ -11,15 +11,14 @@ GOOGLE_SHEET_URL = "<구글주소>"
 CAFE_IDX = GoogleFormSheet.column_code_to_index("Q")
 CMS_IDX = GoogleFormSheet.column_code_to_index("M")
 
-
-class CafeUserDocCheckBox(CafeUserCheckBox):
-    def __init__(self, init_user: CafeUserCheckBox, google_sheet: GoogleFormSheet):
-        super().__init__(init_user._value, init_user._parent)
+class CafeUserDocCheckBox:
+    def __init__(self, checkbox_user: CafeUserCheckBox, google_sheet: GoogleFormSheet):
+        self._user = checkbox_user
         self._sheet = google_sheet
-        _t = self.get().reply.split("/")
+        _t = checkbox_user.get().reply.split("/")
         self._name = _t[1].strip()
         self._phone_last4 = _t[2].strip()
-        self._seq, self._msg = self._load()
+        self._seq, self._level = self._load()
 
     def is_valid(self):
         return self._seq > 0
@@ -27,80 +26,73 @@ class CafeUserDocCheckBox(CafeUserCheckBox):
     def _load(self):
         _find_users = self._sheet.find_keyword_all_with_seq(self._name, re.compile(r".*" + self._phone_last4 + "$"))
         if len(_find_users) != 1:
-            return -1, "[ERROR] 조합원 아님"
-
+            return -1, "비조합원"
         _seq, _user = _find_users[0]
-        if _user[CMS_IDX] != "완료":
-            return -1, "[ERROR] CMS 미등록자"
-        if _user[CAFE_IDX].strip() in ("O", "o", "0", "true"):
-            return -1, "[ERROR] 기존가입자"
-        return _seq, "가입대기자"
+        is_cms = _user[CMS_IDX] == "완료"
+        is_cafe = _user[CAFE_IDX].strip() in ("O", "o", "0", "true")
+
+        if is_cafe:
+            if is_cms:
+                return -1, "중복신청_일반조합원"
+            else:
+                return -1, "중복신청_준조합원"
+        else:
+            if is_cms:
+                return _seq, "일반조합원"
+            else:
+                return _seq, "준조합원"
 
     def is_checked(self):
-        return super().is_checked()
+        return self._user.is_checked()
 
     def checked(self):
-        self._sheet.update_value(self._seq, CAFE_IDX + 1, "true")
-        super().checked()
+        try:
+            self._sheet.update_value(self._seq, CAFE_IDX + 1, "o")
+            self._user.checked()
+        except (Exception, RuntimeError):
+            self._sheet.update_value(self._seq, CAFE_IDX + 1, "x")
 
-    def unchecked(self):
-        self._sheet.update_value(self._seq, CAFE_IDX + 1, "X")
-        # super().unchecked()
+    def level(self):
+        return self._level
+
+    def level_up(self):
+        if self._seq > 0:
+            self._user.level(self._level)
+            return "[SUCCESS] {} (등업: {})".format(self._user, self._level)
+        else:
+            return "[ERROR] {} (등업실패: {})".format(self._user, self._level)
+
+    def user(self):
+        return self._user
 
     def __str__(self):
-        return "({}) {} - {}".format(self._seq, super().__str__(), self._msg)
+        return "({}) {} - {}".format(self._seq, self._user().__str__(), self._level)
 
     def to_dict(self):
-        r = self.get()
+        r = self.user().get()
         return {"id": r.id, "nick": r.nick, "date": r.date,
                 "name": self._name[:1] + "**" if len(self._name) > 1 else "",
-                "last_num": self._phone_last4, "error_msg": self._msg}
+                "last_num": self._phone_last4, "level": self._level}
 
 
 if __name__ == "__main__":
-    msg = ""
-    total_count = 0
-    success_msg = "### 자동승인 대상\n"
-    success_count = 0
-    error_msg = "### 확인요망 대상\n"
-    error_count = 0
     sheet = GoogleFormSheet(url=GOOGLE_SHEET_URL, sheet_name="설문지 응답(CMS포함)")
-    cafe = NCafeAutoJoin(driver_path=DRIVER_PATH).login(USER_ID, PASS_WORD)
-    rollback = []
-    for row in cafe.find_wait_users():
-        total_count += 1
-        cafe_user = CafeUserDocCheckBox(row, sheet)
-        try:
-            if cafe_user.is_valid():
-                cafe_user.checked()
-                success_count += 1
-                rollback.append(cafe_user)
-                success_msg += "[{id}/{nick}] {name}/{last_num} - 신청일: {date}\n".format(**cafe_user.to_dict())
-            else:
-                error_count += 1
-                error_msg += "[{id}/{nick}] {name}/{last_num} - 신청일: {date} : 실패이유: {error_msg}\n" \
-                    .format(**cafe_user.to_dict())
-        except (Exception, RuntimeError):
-            error_count += 1
-            error_msg += "[Exception] {}".format(row)
+    cafe = NCafeAutoJoin(USER_ID, PASS_WORD, DRIVER_PATH, cafe_url="https://cafe.naver.com/nunion")
+    success_msg = "### 자동처리 대상\n"
+    error_msg = "### 수동처리 대상\n"
+    success_cnt = 0
+    error_cnt = 0
+    for _u in cafe.find_wait_users():
+        user = CafeUserDocCheckBox(_u, sheet)
+        if user.is_valid():
+            user.checked()
+            user.level_up()
+            success_cnt += 1
+            success_msg += "[{id}/{nick}] {name}/{last_num} => {level} - 신청일: {date}\n".format(**user.to_dict())
+        else:
+            error_cnt += 1
+            error_msg += "[{id}/{nick}] {name}/{last_num} => {level} - 신청일: {date}\n".format(**user.to_dict())
 
-    is_cafe_save = False
-    try:
-        is_cafe_save = cafe.save()
-        cafe.close()
-    except (Exception, RuntimeError):
-        is_cafe_save = False
-    finally:
-        if success_count > 0 and not is_cafe_save and len(rollback) > 0:
-            for r in rollback:
-                if r.is_valid():
-                    r.unchecked()
-        msg = "## 자동승인 {}명 , 확인필요 {}명 (총: {}명, 저장: {})\n".format(success_count, error_count,
-                                                                 total_count, "O" if is_cafe_save else "X")
-        if success_count > 0 and is_cafe_save:
-            msg += success_msg + "\n"
-        if success_count > 0 and not is_cafe_save:
-            msg += "### 저장실패 " + success_msg + "\n"
-        if error_count > 0:
-            msg += error_msg
-        print(msg)
+    msg = "## 처리현황 (자동처리: {}건, 확인요망: {}건)\n{}\n{}".format(success_cnt, error_cnt, success_msg, error_msg)
+    cafe.close()
+    print(msg)
--- taxdoc/naver_cafe.py
+++ taxdoc/naver_cafe.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
@@ -11,7 +12,7 @@ import time
 CafeUser = namedtuple('CafeUser', 'id nick age gender date reply')
 
 
-class NCafeAutoJoin:
+class NCafeLogin:
     def __init__(self, driver_path, cafe_url="https://cafe.naver.com/nunion"):
         option = webdriver.ChromeOptions()
         option.headless = True
@@ -23,7 +24,46 @@ class NCafeAutoJoin:
         d.implicitly_wait(3)
         self._cafe_url = cafe_url
         self._driver = d
-        self.club_id = 0
+        self._club_id = 0
+        self._username = None
+        self._user_password = None
+        self._driver_path = driver_path
+
+    def login(self, user_name, user_password):
+        """
+        로그인 처리에서 봇으로 판정되면, 캡챠로 이동된다.
+        그래서 우회 로직이 필요하다.
+        """
+        self._username = user_name
+        self._user_password = user_password
+        self._driver.get("https://nid.naver.com/nidlogin.login?url=" + self._cafe_url)
+        self._driver.execute_script("""
+            document.querySelector("#id").value = "{}"; 
+            document.querySelector("#pw").value = "{}";
+        """.format(user_name, user_password))
+        webdriver.ActionChains(self._driver).move_by_offset(712, 384).perform()
+        self.get_element(By.ID, "log.login").click()
+        self._club_id = self._extract_club_id()
+        return self
+
+    def _extract_club_id(self):
+        """
+        로그인후 club 관리 페이지로 이동하면 club_id 를 URL 에서 알아낼수 있다.
+        """
+        self.get_element(By.CLASS_NAME, "ico_setting", True).click()
+        url = self._driver.execute_script("return document.URL")
+        code = "clubid="
+        club_id = url[url.index(code)+len(code):].split("&")[0]
+        return club_id
+
+    def get(self, url):
+        self._driver.get(url)
+
+    def get_driver(self):
+        return self._driver
+
+    def get_club_id(self):
+        return self._club_id
 
     def get_element(self, by, value, wait=False):
         """
@@ -48,83 +88,6 @@ class NCafeAutoJoin:
             self._driver.quit()
         raise Exception("element error !")
 
-    def login(self, user_name, user_password):
-        """
-        로그인 처리에서 봇으로 판정되면, 캡챠로 이동된다.
-        그래서 우회 로직이 필요하다.
-        """
-        self._driver.get("https://nid.naver.com/nidlogin.login?url=" + self._cafe_url)
-        self._driver.execute_script("""
-            document.querySelector("#id").value = "{}"; 
-            document.querySelector("#pw").value = "{}";
-        """.format(user_name, user_password))
-        webdriver.ActionChains(self._driver).move_by_offset(712, 384).perform()
-        self.get_element(By.ID, "log.login").click()
-        self.club_id = self._extract_club_id()
-        return self
-
-    def _extract_club_id(self):
-        """
-        로그인후 club 관리 페이지로 이동하면 club_id 를 URL 에서 알아낼수 있다.
-        """
-        self.get_element(By.CLASS_NAME, "ico_setting", True).click()
-        url = self._driver.execute_script("return document.URL")
-        code = "clubid="
-        club_id = url[url.index(code)+len(code):].split("&")[0]
-        return club_id
-
-    def find_wait_users(self):
-        """
-        유저정보 & 체크박스 컨트롤 가능한 객체 리턴
-        """
-        for user in self._extract_wait_users():
-            yield CafeUserCheckBox(user, self)
-
-    def count(self, checked=None):
-        all_count = len(self._driver.find_elements_by_css_selector("input[name='applyMemberCheck']") or [])
-        if checked is None:
-            return all_count
-
-        checked_count = len(self._driver.find_elements_by_css_selector("input[name='applyMemberCheck']:checked") or [])
-        if checked:
-            return checked_count
-        else:
-            return all_count - checked_count
-
-    def _extract_wait_users(self):
-        """
-            가입승인대기유저 목록 로딩한다
-        """
-        self._driver.get("https://cafe.naver.com/ManageJoinApplication.nhn?search.clubid={}".format(self.club_id))
-        table_body = self.get_element(By.ID, "applymemberList", True)
-        for row in table_body.find_elements_by_css_selector("tr"):
-            if not((row.get_attribute("id") or "").startswith("member")):
-                continue
-            nickname_id = row.find_element_by_css_selector("td:nth-child(2) > div > a").text.strip()\
-                .replace("(", "").replace(")", "").split()
-            age = row.find_element_by_css_selector("td:nth-child(3)").text.strip()
-            gender = row.find_element_by_css_selector("td:nth-child(4)").text.strip()
-            date = row.find_element_by_css_selector("td:nth-child(5)").text.strip()
-            # reply load
-            row.find_element_by_css_selector("td:nth-child(6) > a").click()
-            reply = self.get_element(By.CSS_SELECTOR,
-                                     "#{}Answer>td > div > div > ol > li > p".format(nickname_id[1]), True).text
-            yield CafeUser(id=nickname_id[1], nick=nickname_id[0], age=age, gender=gender, date=date, reply=reply)
-
-    def save(self):
-        if self.count(True) <= 0:
-            return False
-        action = self.get_element(By.CSS_SELECTOR, "div.action_in")
-        for btn in action.find_elements_by_css_selector("a.btn_type"):
-            if btn.text.strip() == "가입승인":
-                btn.click()
-                time.sleep(1)
-                alert = self._driver.switch_to.alert
-                alert.accept()
-                time.sleep(1)
-                return True
-        raise Exception("가입승인 실패")
-
     def close(self):
         if self._driver:
             self._driver.implicitly_wait(1)
@@ -132,12 +95,57 @@ class NCafeAutoJoin:
             self._driver = None
 
 
+class NCafeAutoJoin:
+    def __init__(self, user_name, user_password, driver_path, cafe_url="https://cafe.naver.com/nunion", ):
+        self._accept = NCafeLogin(driver_path=driver_path, cafe_url=cafe_url)\
+            .login(user_name=user_name, user_password=user_password)
+        self._join = NCafeLogin(driver_path=driver_path, cafe_url=cafe_url)\
+            .login(user_name=user_name, user_password=user_password)
+
+    def find_wait_users(self):
+        """
+        유저정보 & 체크박스 컨트롤 가능한 객체 리턴
+        """
+        def _extract_wait_users():
+            """
+                가입승인대기유저 목록 로딩한다
+            """
+            accept = self._accept
+            accept.get("https://cafe.naver.com/ManageJoinApplication.nhn?search.clubid={}".format(accept.get_club_id()))
+            table_body = accept.get_element(By.ID, "applymemberList", True)
+            for row in table_body.find_elements_by_css_selector("tr"):
+                if not ((row.get_attribute("id") or "").startswith("member")):
+                    continue
+                nickname_id = row.find_element_by_css_selector("td:nth-child(2) > div > a").text.strip() \
+                    .replace("(", "").replace(")", "").split()
+                age = row.find_element_by_css_selector("td:nth-child(3)").text.strip()
+                gender = row.find_element_by_css_selector("td:nth-child(4)").text.strip()
+                date = row.find_element_by_css_selector("td:nth-child(5)").text.strip()
+                # reply load
+                row.find_element_by_css_selector("td:nth-child(6) > a").click()
+                reply = accept.get_element(By.CSS_SELECTOR,
+                                           "#{}Answer>td > div > div > ol > li > p".format(nickname_id[1]), True).text
+                yield CafeUser(id=nickname_id[1], nick=nickname_id[0], age=age, gender=gender, date=date, reply=reply)
+        for user in _extract_wait_users():
+            yield CafeUserCheckBox(user, self._accept, self._join)
+
+    def close(self):
+        self._accept.close()
+        self._join.close()
+
+
 class CafeUserCheckBox:
-    def __init__(self, init_user: CafeUser, parent: NCafeAutoJoin):
-        self._parent = parent
+    """
+    가입승인을 해준다
+    """
+    def __init__(self, init_user: CafeUser, accept: NCafeLogin, level_up: NCafeLogin):
+        self._cafe_accept = accept
+        self._level_up = level_up
         self._value = init_user
-        _id = self._value.id
-        self.element = self._parent.get_element(By.CSS_SELECTOR, "input[name='applyMemberCheck'][value='{}']".format(_id))
+        self._id = init_user.id
+        el = self._cafe_accept.get_element(By.CSS_SELECTOR, "input[name='applyMemberCheck'][value='{}']".format(self._id))
+        self._checked = el.is_selected()
+        self.element = el
 
     def set(self, value: CafeUser):
         self._value = value
@@ -146,17 +154,43 @@ class CafeUserCheckBox:
         return self._value
 
     def is_checked(self):
-        return self.element.is_selected()
+        return self._checked
 
     def checked(self):
         if not self.is_checked():
             self.element.click()
+            self._save_for_accept()
+            self._checked = True
 
-    def unchecked(self):
-        if self.is_checked():
-            self.element.click()
+    def level(self, visible_text):
+        level_up = self._level_up
+        seed_url = "https://cafe.naver.com/ManageLevelUp.nhn?m=view&fromUpService=member" \
+                   "&model.clubid={cludid}&model.memberid={id}"
+        level_up.get(seed_url.format(**dict(cludid=level_up.get_club_id(), memberid=self._id)))
+        level_up_select_box = Select(self._driver.find_element_by_id('memberLevelSelect'))
+        level_up_select_box.select_by_visible_text(visible_text)
+        level_up.get_element(By.ID, "comment").send_keys("{} 확인완료".format(visible_text))
+        for btn in self._driver.find_elements_by_css_selector("#pop_footer>a"):
+            if btn.text.strip == "확인":
+                btn.click()
+                time.sleep(1)
+                return True
+        return False
+
+    def _save_for_accept(self):
+        """
+        승인신청
+        """
+        action = self._cafe_accept.get_element(By.CSS_SELECTOR, "div.action_in")
+        for btn in action.find_elements_by_css_selector("a.btn_type"):
+            if btn.text.strip() == "가입승인":
+                btn.click()
+                time.sleep(1)
+                alert = self._cafe_accept.get_driver().switch_to.alert
+                alert.accept()
+                time.sleep(1)
+                return True
+        raise Exception("가입승인 실패 : ".format(self._value))
 
     def __str__(self):
         return"[{}] {}".format("O" if self.is_checked() else "X", self._value)
-
-
