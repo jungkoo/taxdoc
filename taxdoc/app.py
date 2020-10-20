8ec74631af782b672555535fcac933390d09ed81
mc.jeong <mc.jeong@navercorp.com>
2020-10-20 15:01:38 +0900

    다운로드 처리

--- taxdoc/app.py
+++ taxdoc/app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from taxdoc import LoginSession, default_config
+from taxdoc import LoginSession, default_config, ResultRecord
 import os
 from taxdoc.document_builder import DocumentBuilder
-from flask import Flask, request
-
+from flask import Flask, request, send_from_directory
+from flask import send_file
+from flask import make_response
 from taxdoc.tax_api import TaxApi
+import io
+import hashlib
 
 _document_builder = None
 _tax_api = None
@@ -22,7 +25,13 @@ def text_doc():
     print("member_id={}".format(member_id))
     r = _tax_api.get_pay_result(member_id)
     print("r==> {}".format(r))
-    return str(r)
+    doc_id = hashlib.sha256("{}-{}".format(name, number).encode()).hexdigest()
+    print("doc_id=> {}".format(doc_id))
+    result = ResultRecord(doc_id=doc_id, user_name=name, phone_number=number, user_id="",
+                          user_address="", password=None, user_email="", pay_date=r.get("date_range", "-"),
+                          pay_sum=r.get("pay_sum", "0"))
+    _document_builder.save(result=result)
+    return send_from_directory(directory='', filename="{}.pdf".format(doc_id))
 
 
 if __name__ == '__main__':
@@ -30,4 +39,5 @@ if __name__ == '__main__':
     config = default_config()
     _tax_api = TaxApi(LoginSession(user_id=config["LOGIN"]["user"], password=config["LOGIN"]["password"]))
     _document_builder = DocumentBuilder(config)
+
     app.run()
