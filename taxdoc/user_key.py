45c034e2a3e62f42936c8575ed299bdefeed78d4
mc.jeong <mc.jeong@navercorp.com>
2021-01-15 13:00:25 +0900

    최종수정

--- taxdoc/user_key.py
+++ taxdoc/user_key.py
@@ -35,10 +35,18 @@ def normalized_phone_number(src_text):
 
 
 if __name__ == "__main__":
-    domain = "http://0.0.0.0:10080"
-    magic_key = os.environ.get("TAX_DOC_KEY") or "123"
-    data_list = [("정민철", "01025746431", "123")]
+    magic_key = "<매직키>"
+    data_list = """
+0	이름	전화번호	메일주소   
+    """.split("\n")
+    print(len(data_list))
     for data in data_list:
-        k = user_key(data[0], data[1])
-        print("{}\t{}\t{}?code={}".format(data[0], normalized_phone_number(data[1]), domain, k))
+        if len(data.strip()) == 0:
+            continue
+        token = data.split("\t")
+        name = token[1].strip()
+        phone_number = token[2].strip()
+        email = token[3].strip()
+        k = user_key(name=name, phone_number=phone_number, magic_key=magic_key)
+        print("{}\t{}\t{}\thttp://118.67.132.225:10080/?code={}".format(name, phone_number, email, k))
 
